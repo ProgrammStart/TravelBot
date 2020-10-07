d8dba70d041b41142418d32322182aeed201b00c
ProgrammStart <ProgrammStart625@gmail.com>
2020-10-07 14:41:25 +0300

    create CityRepository

---
+++ src/main/java/com/uskoryaev/travelbot/repository/CityRepository.java
@@ -0,0 +1,10 @@
+package com.uskoryaev.travelbot.repository;
+
+import com.uskoryaev.travelbot.domain.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Long> {
+    City findByName(String name);
+}
